d88d77946d1d1ade0584178bbf36778dcff2c152
Lakhvinder Singh <168530266+lakhvinder1664@users.noreply.github.com>
2025-04-27 10:16:58 +0000

    added new things

---
+++ javabasics/numAndmath.js
@@ -0,0 +1,34 @@
+const score = 400
+// console.log(score);
+
+const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(20));
+
+const otherNumber = 123.8966
+
+// console.log(otherNumber.toPrecision(4));
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+// Math
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(6.4));
+// console.log(Math.ceil(6.6));
+// console.log(Math.floor(6.9));
+// console.log(Math.min(3,2,5,9));
+// console.log(Math.max(3,2,5,9));
+
+console.log(Math.random()); //value under 0 & 1
+console.log((Math.random()*10) + 1); 
+console.log((Math.random()*10) + 1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1 )) + max)
